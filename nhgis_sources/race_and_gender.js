d314e06c0e56e702fe49914a751d469e44823dfd
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-05-21 11:12:55 -0400

    Update race data and gender data

--- nhgis_sources/race_and_gender.js
+++ nhgis_sources/race_and_gender.js
@@ -1,6 +1,6 @@
 var config = {
     basePath: 'nhgis_sources/race',
-    header: ['year', 'geography', 'filename', 'total', 'female', 'male', 'white_men', 'black_women', 'pct_women', 'pct_black_women', 'notes'],
+    header: ['year', 'geography', 'filename', 'total', 'female', 'male', 'pct_women', 'black_pct_women', 'white_pct_women', 'notes'],
     nhgisVariables: [
         {
             year: "1890",
@@ -9,6 +9,8 @@ var config = {
             male: ["AV0001", "AV0003", "AV0005", "AV0007"],
             white_men: ["AV0001", "AV0003"],
             black_women: ["AV0008"],
+            black: ["AV0007", "AV0008"],
+            white: ["AV0001", "AV0002", "AV0003", "AV0004", "AV0005", "AV0006"],
             total: ["AV0001", "AV0002", "AV0003", "AV0004", "AV0005", "AV0006", "AV0007", "AV0008"],
             notes: "White includes foreign and native-born."
         },
@@ -16,6 +18,7 @@ var config = {
             year: "1900",
             filename: "nhgis0008_ds31_1900_county.csv",
             black_women: ["AZ3004"],
+            black: ["AZ3003", "AZ3004"],
             total: ["AZ3001", "AZ3002", "AZ3003", "AZ3004", "AZ2001"],
             notes: "Need to download white pop by sex still."
         },
@@ -26,6 +29,8 @@ var config = {
             male: ["A30001", "A30003"],
             white_men: ["A30001"],
             black_women: ["A30004"],
+            black: ["A30003", "A30004"],
+            white: ["A30001", "A30002"],
             total: ["A30001", "A30002", "A30003", "A30004"]
         },
         {
@@ -35,6 +40,8 @@ var config = {
             male: ["A8L001","A8L003", "A8L005"],
             white_men: ["A8L001", "A8L003"],
             black_women: ["A8L006"],
+            black: ["A8L005", "A8L006"],
+            white: ["A8L001", "A8L002", "A8L003", "A8L004"],
             total: ["A8L001", "A8L002", "A8L003", "A8L004", "A8L005", "A8L006"],
             notes: "White includes foreign and native-born."
         },
@@ -45,6 +52,8 @@ var config = {
             male: ["BDK001","BDK003", "BDK005"],
             white_men: ["BDK001"],
             black_women: ["BDK004"],
+            black: ["BDK003", "BDK004"],
+            white: ["BDK001", "BDK002"],
             total: ["BDK001", "BDK002", "BDK003", "BDK004", "BDK005", "BDK006"]
         },
         {
@@ -54,6 +63,8 @@ var config = {
             male: ["BV2001","BV2003", "BV2005"],
             white_men: ["BV2001"],
             black_women: ["BV2004"],
+            black: ["BV2003", "BV2004"],
+            white: ["BV2001", "BV2002"],
             total: ["BV2001", "BV2002", "BV2003", "BV2004", "BV2005", "BV2006"]
         },
         {
@@ -62,6 +73,7 @@ var config = {
             female: ["B1Z002", "B1Z004"],
             male: ["B1Z001","B1Z003"],
             white_men: ["B1Z001"],
+            white: ["B1Z001", "B1Z002"],
             total: ["B1Z001", "B1Z002", "B1Z003", "B1Z004"],
             notes: "For this year the only race categories were white and non-white."
         },
@@ -72,6 +84,8 @@ var config = {
             male: ["B5S001","B5S003", "B5S005", "B5S007", "B5S009", "B5S011", "B5S013"],
             white_men: ["B5S001"],
             black_women: ["B5S009"],
+            black: ["B5S002", "B5S009"],
+            white: ["B5S001", "B5S008"],
             total: ["B5S001", "B5S002", "B5S003", "B5S004", "B5S005", "B5S006", "B5S007", "B5S008", "B5S009", "B5S010", "B5S011", "B5S012", "B5S013", "B5S014"],
             notes: "This is the year racial categories start to get more detailed again"
         },
@@ -82,6 +96,8 @@ var config = {
             male: ["B21AA1970", "B21AB1970", "B21AC1970", "B21AD1970", "B21AE1970"],
             white_men: ["B21AA1970"],
             black_women: ["B21AG1970"],
+            black: ["B21AB1970", "B21AG1970"],
+            white: ["B21AA1970", "B21AF1970"],
             total: ["B21AA1970", "B21AB1970", "B21AC1970", "B21AD1970", "B21AE1970", "B21AF1970", "B21AG1970", "B21AH1970", "B21AI1970", "B21AJ1970"]
         },
         {
@@ -91,8 +107,10 @@ var config = {
             male: ["AZ8AA1980", "AZ8AB1980", "AZ8AC1980", "AZ8AD1980", "AZ8AE1980"],
             white_men: ["AZ8AA1980"],
             black_women: ["AZ8AG1980"],
+            black: ["AZ8AB1980", "AZ8AG1980"],
+            white: ["AZ8AA1980", "AZ8AF1980"],
             total: ["AZ8AA1980", "AZ8AB1980", "AZ8AC1980", "AZ8AD1980", "AZ8AE1980", "AZ8AF1980", "AZ8AG1980", "AZ8AH1980", "AZ8AI1980", "AZ8AJ1980"],
-            notes: "Excludes 'other race'"
+            notes: "Excludes 'other race'. White includes Hispanic"
         },
         {
             year: "1990",
@@ -101,7 +119,10 @@ var config = {
             male: ["B21AA1990", "B21AB1990", "B21AC1990", "B21AD1990", "B21AE1990"],
             white_men: ["B21AA1990"],
             black_women: ["B21AG1990"],
-            total: ["B21AA1990", "B21AB1990", "B21AC1990", "B21AD1990", "B21AE1990", "B21AF1990", "B21AG1990", "B21AH1990", "B21AI1990", "B21AJ1990"]
+            black: ["AZ8AB1990", "AZ8AG1990"],
+            white: ["AZ8AA1990", "AZ8AF1990"],
+            total: ["B21AA1990", "B21AB1990", "B21AC1990", "B21AD1990", "B21AE1990", "B21AF1990", "B21AG1990", "B21AH1990", "B21AI1990", "B21AJ1990"],
+            notes: "White includes hispanic"
         },
         {
             year: "2000",
@@ -110,7 +131,10 @@ var config = {
             male: ["B21AA2000", "B21AB2000", "B21AC2000", "B21AD2000", "B21AE2000"],
             white_men: ["B21AA2000"],
             black_women: ["B21AG2000"],
-            total: ["B21AA2000", "B21AB2000", "B21AC2000", "B21AD2000", "B21AE2000", "B21AF2000", "B21AG2000", "B21AH2000", "B21AI2000", "B21AJ2000"]
+            black: ["AZ8AB2000", "AZ8AG2000"],
+            white: ["AZ8AA2000", "AZ8AF2000"],
+            total: ["B21AA2000", "B21AB2000", "B21AC2000", "B21AD2000", "B21AE2000", "B21AF2000", "B21AG2000", "B21AH2000", "B21AI2000", "B21AJ2000"],
+            notes: "White includes hispanic"
         },
         {
             year: "2010",
@@ -119,15 +143,21 @@ var config = {
             male: ["B21AA2010", "B21AB2010", "B21AC2010", "B21AD2010", "B21AE2010"],
             white_men: ["B21AA2010"],
             black_women: ["B21AG2010"],
-            total: ["B21AA2010", "B21AB2010", "B21AC2010", "B21AD2010", "B21AE2010", "B21AF2010", "B21AG2010", "B21AH2010", "B21AI2010", "B21AJ2010"]
+            black: ["AZ8AB2010", "AZ8AG2010"],
+            white: ["AZ8AA2010", "AZ8AF2010"],
+            total: ["B21AA2010", "B21AB2010", "B21AC2010", "B21AD2010", "B21AE2010", "B21AF2010", "B21AG2010", "B21AH2010", "B21AI2010", "B21AJ2010"],
+            notes: "White includes hispanic"
         }
     ],
     derivedVariables: {
         pct_women: function (row) {
             return (100 * (row.total > 0 ? row.female / row.total : 0)).toFixed(4);
         },
-        pct_black_women: function(row) {
-            return (row.total > 0 ? 100 * (row.black_women / row.total) : 0).toFixed(4);
+        black_pct_women: function(row) {
+            return (row.black > 0 ? 100 * (row.black_women / row.black) : 0).toFixed(4);
+        },
+        white_pct_women: function(row) {
+            return (row.white > 0 ? (100 - 100 * (row.white_men / row.white)) : 0).toFixed(4);
         }
     },
     geographyFilterCallbacks: {
